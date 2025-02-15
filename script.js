08efb95b95e1e37995e6676e7000cefb08553820
Prince Saini <princesaini0144@gmail.com>
2025-02-15 22:48:33 +0530

    add recaptcha required

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-function beforeSubmit() {
-    let outputDate = document.querySelector('.outputDate');
-    let inputDate = document.querySelector('.inputDate');
+let recaptcha = false;
 
-    if (inputDate.value) {
-        // let formattedDate = new Date(inputDate.value).toISOString().split('T')[0]; // YYYY-MM-DD format
-        // outputDate.value = formattedDate;
-        // console.log("Formatted Lead Date:", outputDate.value);
-        // return true; // Allow form submission
-        let dateObj = new Date(inputDate.value);
-        let day = String(dateObj.getDate()).padStart(2, '0'); // Ensure two-digit day
-        let year = dateObj.getFullYear();
+function beforeSubmit(event) {
 
-        let formattedDate = `${day}/${month}/${year}`; // Format: dd/mm/yyyy
-        outputDate.value = formattedDate;
-        console.log("Formatted Lead Date: ", outputDate.value);
-        return true;
-    } else {
-        console.error("No Lead Date selected");
-        alert("Please select a Lead Date before submitting.");
-        return false; // Prevent form submission
+    if(recaptcha){
+        let outputDate = document.querySelector('.outputDate');
+        let inputDate = document.querySelector('.inputDate');
+    
+        if (inputDate.value) {
+            // let formattedDate = new Date(inputDate.value).toISOString().split('T')[0]; // YYYY-MM-DD format
+            // outputDate.value = formattedDate;
+            // console.log("Formatted Lead Date:", outputDate.value);
+            // return true; // Allow form submission
+            let dateObj = new Date(inputDate.value);
+            let day = String(dateObj.getDate()).padStart(2, '0'); // Ensure two-digit day
+            let year = dateObj.getFullYear();
+    
+            let formattedDate = `${day}/${month}/${year}`; // Format: dd/mm/yyyy
+            outputDate.value = formattedDate;
+            console.log("Formatted Lead Date: ", outputDate.value);
+            return true;
+        } else {
+            console.error("No Lead Date selected");
+            alert("Please select a Lead Date before submitting.");
+            return false; // Prevent form submission
+            
+        }
     }
+    else{
+        alert('Please select the Recaptcha box to submit the form');
+        event.preventDefault();
+    }
+
 }
 
 
@@ -30,4 +41,10 @@ function timestamp() {
         elems["ts"] = JSON.stringify(new Date().getTime()); 
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
     }
-} setInterval(timestamp, 500); 
+} 
+setInterval(timestamp, 500); 
+
+
+function captchaSuccess(){
+    recaptcha = true;
+}
