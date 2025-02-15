4e0ca9c7036d827156177417328c0934e42e2929
Prince Saini <princesaini0144@gmail.com>
2025-02-15 22:38:17 +0530

    add the capcha

--- script.js
+++ script.js
@@ -12,12 +12,22 @@ function beforeSubmit() {
         let year = dateObj.getFullYear();
 
         let formattedDate = `${day}/${month}/${year}`; // Format: dd/mm/yyyy
-        outputDate.value = formattedDate; 
+        outputDate.value = formattedDate;
         console.log("Formatted Lead Date: ", outputDate.value);
-        return true; 
+        return true;
     } else {
         console.error("No Lead Date selected");
         alert("Please select a Lead Date before submitting.");
         return false; // Prevent form submission
     }
 }
+
+
+function timestamp() {
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value); 
+        elems["ts"] = JSON.stringify(new Date().getTime()); 
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    }
+} setInterval(timestamp, 500); 
